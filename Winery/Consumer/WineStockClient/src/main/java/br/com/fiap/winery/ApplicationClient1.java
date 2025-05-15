861302ad64ef2e19f447a28c9163f8db8af6ecd2
vitorisacb <vitorisac14@gmail.com>
2025-05-15 11:39:26 -0300

    27 - 29

--- Winery/Consumer/WineStockClient/src/main/java/br/com/fiap/winery/ApplicationClient1.java
+++ Winery/Consumer/WineStockClient/src/main/java/br/com/fiap/winery/ApplicationClient1.java
@@ -1,11 +1,14 @@
 package br.com.fiap.winery;
 
-import java.net.URL; // Importação necessária
-// Outros imports virão...
+import java.net.URL;
+import javax.xml.namespace.QName;
+import jakarta.xml.ws.Service;
 
 public class ApplicationClient1 {
     public static void main(String[] args) throws Exception {
         URL url = new URL("<http://localhost:8085/WineStockService?wsdl>");
-        // ...
+
+        QName qName = new QName("<http://winery.fiap.com.br/>", "WineStockServiceImplementationService");
+        Service serviceFactory = Service.create(url, qName);
     }
 }
